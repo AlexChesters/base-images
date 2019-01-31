95d6a90c4c351667e944fcbca842a43a30d088d2
Alex Chesters <alex.chesters@bbc.co.uk>
2019-01-31 12:17:17 +0000

    done a codebuild

--- cdk/stack.ts
+++ cdk/stack.ts
@@ -1,7 +1,26 @@
+import * as codebuild from '@aws-cdk/aws-codebuild'
 import * as cdk from '@aws-cdk/cdk'
 
 export default class Stack extends cdk.Stack {
   constructor (parent: cdk.App, name: string, props?: cdk.StackProps) {
     super(parent, name, props)
+
+    new codebuild.Project(this, 'Project', {
+      buildSpec: {
+        phases: {
+          build: {
+            commands: [
+              'ls -al'
+            ]
+          }
+        },
+        version: '0.2'
+      },
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.UBUNTU_14_04_BASE,
+        computeType: codebuild.ComputeType.Small
+      },
+      projectName: 'base-images'
+    })
   }
 }
