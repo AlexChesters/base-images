09c8bb07bc187a7d322203ba2b570933449020b3
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2019-12-18 13:24:00 +0000

    fix nginx build

--- ci/scripts/build-all-images.sh
+++ ci/scripts/build-all-images.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
 
-# concurrently \
-#   'packer build images/docker/packer.json' \
-#   'packer build images/nginx/packer.json' \
-#   'packer build images/nodejs/packer.json'
-
-packer build images/nginx/packer.json
+concurrently \
+  'packer build images/docker/packer.json' \
+  'packer build images/nginx/packer.json' \
+  'packer build images/nodejs/packer.json'
--- images/nginx/scripts/002-install-nginx.sh
+++ images/nginx/scripts/002-install-nginx.sh
@@ -1,6 +1,5 @@
 set -e
 
 amazon-linux-extras install nginx1.12
-mv /tmp/nginx.conf /etc/nginx/nginx.conf
 
 systemctl enable nginx
