5dacc0208921da84f819c5971dfc1c8f655b4959
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2020-02-06 17:25:25 +0000

    build sequentially to debug

--- ci/scripts/build-all-images.sh
+++ ci/scripts/build-all-images.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
-concurrently \
-  'packer build images/docker/packer.json' \
-  'packer build images/nginx/packer.json' \
-  'packer build images/nodejs/packer.json'
+# concurrently \
+#   'packer build images/docker/packer.json' \
+#   'packer build images/nginx/packer.json' \
+#   'packer build images/nodejs/packer.json'
+
+packer build images/docker/packer.json
+packer build images/nginx/packer.json
+packer build images/nodejs/packer.json
