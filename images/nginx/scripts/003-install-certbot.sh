77137be28fb9e2492746f95978880f9a9894bb33
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2020-10-13 21:40:02 +0100

    install certbot

--- images/nginx/scripts/003-install-certbot.sh
+++ images/nginx/scripts/003-install-certbot.sh
@@ -1,6 +1,3 @@
 set -e
 
-wget https://dl.eff.org/certbot-auto
-mv certbot-auto /usr/local/bin/certbot-auto
-chown root /usr/local/bin/certbot-auto
-chmod 0755 /usr/local/bin/certbot-auto
+yum install -y certbot
